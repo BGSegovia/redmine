907d5ae7eaf76ddad3843afc366cc438e85bc6ea
Beatriz Garcia <beku84.garcia@gmail.com>
2012-04-22 06:05:02 +0200

    first commmit in redmine

---
+++ projects_tree_view/init.rb
@@ -0,0 +1,25 @@
+require 'redmine'
+
+# Patches to the Redmine core.
+require 'dispatcher'
+require 'projectstreeview_projects_helper_patch'
+Dispatcher.to_prepare do
+  ProjectsHelper.send(:include, ProjectstreeviewProjectsHelperPatch)
+end
+
+Redmine::Plugin.register :projects_tree_view do
+  name 'Projects Tree View plugin'
+  author 'Chris Peterson'
+  description 'This is a Redmine plugin which will turn the projects page into a tree view'
+  version '0.0.3'
+end
+
+class ProjectsTreeViewListener < Redmine::Hook::ViewListener
+
+  # Adds javascript and stylesheet tags
+  def view_layouts_base_html_head(context)
+    javascript_include_tag('projects_tree_view', :plugin => :projects_tree_view) +
+    stylesheet_link_tag('projects_tree_view', :plugin => :projects_tree_view)
+  end
+  
+end
